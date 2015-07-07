7b224c32ffe5f4b7e9b0b9e6f1eabee8f765c0d9
Hg <dev@indigo.re>
2015-07-07 23:34:41 +0200

    add datetime operations with durations

--- durations.py
+++ durations.py
@@ -1,6 +1,7 @@
 
 import sys
 import re
+import datetime
 
 def duractiondict_to_seconds(d):
 	get = lambda k: d.get(k, 0)
@@ -29,13 +30,13 @@ def seconds_to_durationdict(secs):
 		ratio, secs = divmod(secs, seconds_units[unit])
 		if ratio >= 1:
 			result[unit] = ratio
-	
+
 	millis_s, secs = math.modf(secs)
 	if secs:
 		result['seconds'] = int(secs)
 	if millis_s:
 		result['milliseconds'] = int(millis_s * 1000)
-	
+
 	return result
 
 def duractiondict_to_string(d):
@@ -72,7 +73,7 @@ class ParserException(Exception):
 class ExtraDataException(ParserException):
 	def __init__(self, *a, **kw):
 		ParserException.__init__(self, *a, **kw)
-		self.message = 'bad'
+		self.message = 'Expected no tokens after token %s but got %s' % (self.after_token, self.bad_token)
 
 class UnexpectedTokenException(ParserException):
 	pass
@@ -81,6 +82,13 @@ class BadOperandException(ParserException):
 	def __init__(self, operator=None, *a, **kw):
 		ParserException.__init__(self, *a, **kw)
 		self.operator = operator
+		self.message = 'Unsupported operator %r between %s and %s' % (self.operator, self.after_token, self.bad_token)
+
+class CombinedException(ParserException):
+	def __init__(self, *a, **kw):
+		ParserException.__init__(self, *a, **kw)
+		self.message = 'Cannot combine 2 similar %s items: %s and %s' % (self.after_token.type, self.after_token, self.bad_token)
+
 
 class Parser:
 	def __init__(self, tokens):
@@ -132,6 +140,9 @@ class Parser:
 			dsum = sum(durations, Duration())
 			return dsum, s
 		elif self.tokens[start].type == 'datetime':
+			if start + 1 < end and self.tokens[start + 1].type == 'datetime':
+				tok = Datetime.combine(self.tokens[start], self.tokens[start + 1])
+				return tok, start + 2
 			return self.tokens[start], start + 1
 		elif self.tokens[start].type == 'factor':
 			return self.tokens[start], start + 1
@@ -182,26 +193,7 @@ dur_text_re = re.compile(
 ")")
 
 
-
-dur_colon_re = re.compile(
-"(?:(?P<days>\d+):)?"
-"(?:(?P<hours>\d+):)?"
-"(?:(?P<minutes>\d+):)?"
-"(?P<seconds>\d+)"
-"(?:[.,](?P<milliseconds>\d+))?$")
-
-dur_colon_re = re.compile(
-"(?:"
-	"(?:"
-		"(?:(?P<days_2>\d+):)?"
-		"(?:(?P<hours_2>\d+):)"
-	")?"
-	"(?:(?P<minutes_2>\d+):)"
-")?"
-"(?P<seconds_2>\d+)"
-"(?:[.,](?P<milliseconds_2>\d+))?")
-
-duration_re = re.compile("(?P<duration>(?=.)%s)" % '|'.join([dur_text_re.pattern]))# colon_re.pattern]))
+duration_re = re.compile("(?P<duration>%s)" % dur_text_re.pattern)
 
 class Duration:
 	units = 'years months weeks days hours minutes seconds milliseconds'.split()
@@ -216,22 +208,51 @@ class Duration:
 		for unit in self.units:
 			self.items[unit] = float(d.get(unit, 0))
 
+	def timedelta(self):
+		days = self.items.get('years') * 365 + self.items.get('months') * 30 + self.items.get('weeks') * 7 + self.items.get('days')
+		seconds = self.items.get('hours') * 3600 + self.items.get('minutes') * 60 + self.items.get('seconds')
+		return datetime.timedelta(days=days, seconds=seconds, microseconds=self.items.get('milliseconds') * 1000)
+
+	def set_timedelta(self, delta):
+		factor = 1
+		if delta < datetime.timedelta():
+			factor = -1
+			delta = -delta
+
+		days = delta.days
+		self.items['years'], days = divmod(days, 365)
+		self.items['months'], days = divmod(days, 30)
+		self.items['weeks'], days = divmod(days, 7)
+		self.items['days'] = days
+
+		seconds = delta.seconds
+		self.items['hours'], seconds = divmod(seconds, 3600)
+		self.items['minutes'], seconds = divmod(seconds, 60)
+		self.items['seconds'] = seconds
+
+		self.items['milliseconds'] = delta.microseconds / 1000
+
+		for unit in self.units:
+			self.items[unit] *= factor
+
 	def __add__(self, other):
 		if other.type == 'duration':
 			new = Duration()
-			for unit in self.units:
-				new.items[unit] = self.items.get(unit) + other.items.get(unit)
+#			for unit in self.units:
+#				new.items[unit] = self.items.get(unit) + other.items.get(unit)
+			new.set_timedelta(self.timedelta() + other.timedelta())
 			return new
 		elif other.type == 'datetime':
-			pass
+			return other + self
 
 		raise BadOperandException('+', self, other)
 
 	def __sub__(self, other):
 		if other.type == 'duration':
 			new = Duration()
-			for unit in self.units:
-				new.items[unit] = self.items.get(unit) - other.items.get(unit)
+#			for unit in self.units:
+#				new.items[unit] = self.items.get(unit) - other.items.get(unit)
+			new.set_timedelta(self.timedelta() - other.timedelta())
 			return new
 
 		raise BadOperandException('-', self, other)
@@ -239,7 +260,10 @@ class Duration:
 	def merge(self, other):
 		new = Duration()
 		for unit in self.units:
-			pass
+			if self.items.get(unit) and other.items.get(unit):
+				raise CombinedException(self, other)
+			new.items[unit] = self.items.get(unit) + other.items.get(unit)
+		return new
 
 	def __repr__(self):
 		parts = ['%s %s' % (self.items.get(u), u) for u in self.units if self.items.get(u)]
@@ -255,8 +279,7 @@ class Duration:
 		return d
 
 
-
-op_re = re.compile(r"(?P<op>[-+*/()])")
+op_re = re.compile(r"(?P<op>[-+*()])\s*")
 
 class Operator:
 	type = 'operator'
@@ -313,50 +336,118 @@ class Factor:
 	def __repr__(self):
 		return '<Factor=%s>' % self.factor
 
-
 date_re = re.compile(
 "(?P<dt_day>"
 	"(?P<date_lit>today)|"
-	"(?P<year>\d{2,4})(?P<_datesep>/?)(?P<month>\d{1,2})(?P=_datesep)(?P<day>\d{1,2})"
-"\s*)")
+	"(?P<year>\d{2,4})(?P<_datesep>[/-]?)(?P<month>\d{1,2})(?P=_datesep)(?P<day>\d{1,2})"
+")\s*")
 time_re = re.compile(
 "(?P<dt_time>"
 	"(?P<hour>\d{2})(?P<_timesep>:?)(?P<minute>\d{2})(?:(?P=_timesep)(?P<second>\d{2}))?|"
 	"(?P<hour_2>\d{1,2})(?::(?P<minute_2>\d{1,2}))?\s*(?P<ampm>am|pm)"
-"\s*)")
+")\s*")
 
-datetime_re = re.compile(r'(?P<datetime>%s)' % date_re.pattern)
+datetime_re = re.compile(r'(?P<datetime>%s|%s)' % (date_re.pattern, time_re.pattern))
 
 class Datetime:
 	type = 'datetime'
 	units = 'year month day hour minute second'.split()
 
-	def __init__(self, d):
-		d = tweak_dict(d)
+	def __init__(self, d=None):
+		d = tweak_dict(d or {})
 
+		self.items = {}
 		for unit in self.units:
-			setattr(self, unit, int(d.get(unit, 0)))
+			self.items[unit] = int(d.get(unit, 0))
 		ampm = d.get('ampm')
 		if ampm:
-			if ampm == 'am' and self.hour == 12:
-				self.hour = 0
-			elif ampm == 'pm' and self.hour != 12:
-				self.hour += 12
+			if ampm == 'am' and self.items['hour'] == 12:
+				self.items['hour'] = 0
+			elif ampm == 'pm' and self.items['hour'] != 12:
+				self.items['hour'] += 12
+
+	@classmethod
+	def combine(cls, a, b):
+		res = Datetime()
+		for unit in cls.units:
+			if a.items.get(unit) and b.items.get(unit):
+				raise CombinedException(a, b)
+			res.items[unit] = a.items.get(unit) + b.items.get(unit)
+		return res
+
+	def datetime(self):
+		return datetime.datetime(self.items['year'], self.items['month'], self.items['day'], self.items['hour'], self.items['minute'], self.items['second'])
+
+	def set_datetime(self, dt):
+		for unit in self.units:
+			self.items[unit] = getattr(dt, unit)
 
 	def __mul__(self, other):
 		raise BadOperandException('*', self, other)
 
 	def __add__(self, other):
 		if other.type == 'duration':
-			return other + self
+			return self._add_duration(other, True)
+
 		raise BadOperandException('+', self, other)
+
+	def _add_duration(self, other, positive):
+			new = Datetime()
+			dt = self.datetime()
+			delta_args = {}
+			for unit in ('weeks', 'days', 'hours', 'minutes', 'seconds', 'milliseconds'):
+				delta_args[unit] = other.items.get(unit)
+			delta = datetime.timedelta(**delta_args)
+
+			if positive:
+				dt += delta
+				year, month = divmod(dt.month + other.items.get('months') - 1, 12)
+				year += dt.year + other.items.get('years')
+			else:
+				dt -= delta
+				year, month = divmod(dt.month - other.items.get('months') - 1, 12)
+				year += dt.year - other.items.get('years')
+
+			dt = dt.replace(year=int(year), month=int(month + 1))
+			new.set_datetime(dt)
+			return new
+
 
 	def __sub__(self, other):
 		if other.type == 'duration':
-			pass
+			return self._add_duration(other, False)
+		elif other.type == 'datetime':
+			d_self = self.datetime()
+			m_self = d_self.year * 12 + d_self.month
+			d_other = other.datetime()
+			m_other = d_other.year * 12 + d_other.month
+
+			years, months = divmod(m_self - m_other, 12)
+			#d_self = d_self.replace(year
+
+			delta = self.datetime() - other.datetime()
+			res = Duration()
+
+			res.set_timedelta(delta)
+			return res
+
+			# negative timedelta still have positive sec and usec
+			factor = 1
+			if delta < datetime.timedelta():
+				factor = -1
+				delta = -delta
+
+			res.items['days'] = delta.days * factor
+			res.items['seconds'] = delta.seconds * factor
+			res.items['milliseconds'] = delta.microseconds * factor / 1000
+
+			return res
+
 		raise BadOperandException('-', self, other)
 
-		
+	def __repr__(self):
+		return '<Datetime %r>' % self.datetime()
+
 
 full_re = re.compile('|'.join([datetime_re.pattern, duration_re.pattern, factor_re.pattern, op_re.pattern]))
 
@@ -375,14 +466,14 @@ def gen_token(tok):
 def main():
 	tokens = []
 	for mtc in full_re.finditer(sys.argv[1]):
-		print mtc.group(0)
 		tokens.append(gen_token(mtc.groupdict()))
-	print tokens
 	res = Parser(tokens).parse()
 	if res.type == 'operator':
-		print res
 		res = res.apply()
-	print res
+	if res.type == 'datetime':
+		print str(res.datetime())
+	else:
+		print res
 
 
 if __name__ == '__main__':
